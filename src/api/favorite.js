8e0114beb7994ef62b1a3bb6e63edaecf3216fad
Cris <cristopher.26hp@gmail.com>
2024-03-26 15:54:05 -0600

    Se creo funcion para eliminar de favoritos, el icono de favs ya hace un efecto cuando se agega o se quita de favoritos

--- src/api/favorite.js
+++ src/api/favorite.js
@@ -5,7 +5,8 @@ import { FAVORITE_STORAGE } from "../utils/constants";
 export async function getPokemonsFavoriteApi() {
     try {
         const response = await AsyncStorage.getItem(FAVORITE_STORAGE);
-        return response;
+        return JSON.parse(response || "[]");
+        //return response ? JSON.parse(response) : [];
     } catch (error) {
         throw error;
     }
@@ -13,9 +14,44 @@ export async function getPokemonsFavoriteApi() {
 
 export async function addPokemonFavoriteApi(id) {
     try {
-        const favorites = [];
+        //const favorites = [];
+        // favorites.push(id);
+        // const response = JSON.parse(await AsyncStorage.getItem(FAVORITE_STORAGE));
+        // console.log(response, "ressss")
+        // let newFavs;
+        // if (response?.length > 0) {
+        //     let arr =  [...response, ...favorites];
+        //     const arrSinRepetidos = new Set(arr);
+        //     newFavs = [...arrSinRepetidos];
+        // } else {
+        //     newFavs = [...favorites];
+        // }
+        // console.log("FAV", newFavs)
+        const favorites = await getPokemonsFavoriteApi();
         favorites.push(id);
+        console.log("Favorites", favorites)
         await AsyncStorage.setItem(FAVORITE_STORAGE, JSON.stringify(favorites));
+    } catch (error) {
+        throw error;
+    }
+}
+
+export async function isPokemonFavorite(id) {
+    try {
+        const response = await getPokemonsFavoriteApi();
+        const isFav = includes(response, id);
+        //const isFav = response?.includes(id);
+        return isFav;
+    } catch (error) {
+        throw error;
+    }
+}
+
+export async function removePokemonFavoriteApi(id) {
+    try {
+        const favorites = await getPokemonsFavoriteApi();
+        const newFavorites = pull(favorites, id);
+        await AsyncStorage.setItem(FAVORITE_STORAGE, JSON.stringify(newFavorites));
     } catch (error) {
         throw error;
     }
--- src/components/Pokemon/Favorite.js
+++ src/components/Pokemon/Favorite.js
@@ -1,19 +1,52 @@
-import React from 'react'
-import Icon from 'react-native-vector-icons/FontAwesome5'
-import { addPokemonFavoriteApi, getPokemonsFavoriteApi } from '../../api/favorite';
+import React, { useState, useEffect } from 'react';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import { addPokemonFavoriteApi, isPokemonFavorite, removePokemonFavoriteApi } from '../../api/favorite';
 
 export default function Favorite(props) {
     const { id } = props;
+    const [isFavorite, setIsFavorite] = useState(undefined);
+    const [reloadCheck, setReloadCheck] = useState(false);
+    const Icon = isFavorite ? FontAwesome : FontAwesome5;
+
+    useEffect(() => {
+        (async () => {
+            try {
+                const response = await isPokemonFavorite(id);
+                setIsFavorite(response);
+            } catch (error) {
+                setIsFavorite(false);
+            }
+        })()
+    }, [id, reloadCheck]);
+
+    const onReloadCheckFavorite = () => {
+        setReloadCheck((prev) => !prev);
+    }
+
     const addFavorite = async () => {
-        console.log(id)
-        await addPokemonFavoriteApi(id);
+        try {
+            await addPokemonFavoriteApi(id);
+            onReloadCheckFavorite();
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    const removeFavorite = async () => {
+        try {
+            await removePokemonFavoriteApi(id);
+            onReloadCheckFavorite();
+        } catch (error) {
+            console.error(error)
+        }
     }
     return (
         <Icon
             name="heart"
             color="#fff"
             size={30}
-            onPress={addFavorite}
+            onPress={isFavorite ? removeFavorite : addFavorite}
             style={{ marginRight: 20, }}
         />
     )
