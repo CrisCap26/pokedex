896ccfda94b381d7ae4bdfb80bb6bab1a656f166
Cris <cristopher.26hp@gmail.com>
2024-03-27 08:46:36 -0600

    Creando vista de favoritos para que aparezcan los pokemons agregados a favs

--- src/screens/Favorite.js
+++ src/screens/Favorite.js
@@ -1,16 +1,44 @@
-import React from 'react'
-import { View, Text, Button } from 'react-native'
+import React, { useState, useCallback } from 'react'
+import { Text } from 'react-native'
+import { useFocusEffect } from '@react-navigation/native'
 import { getPokemonsFavoriteApi } from '../api/favorite'
+import { getPokemonDetailsApi } from '../api/pokemon'
+import useAuth from '../hooks/useAuth'
+import PokemonList from '../components/PokemonList'
 
 export default function Favorite() {
-  const checkFavorites = async () => {
-    const response = await getPokemonsFavoriteApi();
-    console.log(response);
-  }
-  return (
-    <View>
-      <Text>Favorite</Text>
-      <Button title='Get favorites' onPress={checkFavorites}/>
-    </View>
+  const [pokemons, setPokemons] = useState([]);
+  const { auth } = useAuth();
+
+  useFocusEffect(
+    useCallback(() => {
+      if (auth) {
+        (async () => {
+          const response = await getPokemonsFavoriteApi();
+          console.log(response, "UseEff")
+
+          const pokemonsArray = [];
+          for await (const id of response) {
+            const pokemonDetail = await getPokemonDetailsApi(id);
+
+            pokemonsArray.push({
+              id: pokemonDetail.id,
+              name: pokemonDetail.name,
+              type: pokemonDetail.types[0].type.name,
+              order: pokemonDetail.order,
+              image: pokemonDetail.sprites.other['official-artwork'].front_default,
+            });
+          }
+
+          setPokemons(pokemonsArray);
+        })()
+      }
+    }, [auth])
   )
+
+
+  return !auth ?
+    (<Text>Usuario no logueado</Text>)
+    :
+    (<PokemonList pokemons={pokemons} />)
 }
